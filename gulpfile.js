d532519cd8f4d5e32058b3d1dba015e314fd6dd4
Marco McNulty <marcomcnultycodes@gmail.com>
2021-06-26 19:11:17 +0100

    📦 NEW: Fonts setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const {parallel, watch} = require('gulp');
+const fonts = require('./gulp-tasks/fonts.js');
 
 // Pull in each task
 const sass = require('./gulp-tasks/sass.js');
@@ -12,7 +13,7 @@ const watcher = () => {
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parrallel
-exports.default = parallel(sass);
+exports.default = parallel(fonts, sass);
 
 // This is our watcher task that instructs gulp to watch directories and
 // act accordingly
